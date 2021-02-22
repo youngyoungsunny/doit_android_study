cc2eeb37799f9e747b31f7c32e151c27f375cc1f
SeonYoungKim98 <kimksy9@gmail.com>
2021-02-22 23:08:45 +0900

    02-22

---
+++ android_study/app/src/main/java/com/smu/android_study/MenuActivity.java
@@ -0,0 +1,54 @@
+package com.smu.android_study;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button customButton = findViewById(R.id.button2);
+        Button resultButton = findViewById(R.id.button3);
+        Button mdButton = findViewById(R.id.button4);
+
+        customButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("name","고객 관리 메뉴 클릭됨!");
+
+                setResult(Activity.RESULT_OK, intent); // 돌려줄 결과 저장
+                finish();
+            }
+        });
+
+        resultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("name","매출 관리 메뉴 클릭됨!");
+
+                setResult(Activity.RESULT_OK, intent); // 돌려줄 결과 저장
+                finish();
+            }
+        });
+
+        mdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("name","상품 관리 메뉴 클릭됨!");
+
+                setResult(Activity.RESULT_OK, intent); // 돌려줄 결과 저장
+                finish();
+            }
+        });
+    }
+}
